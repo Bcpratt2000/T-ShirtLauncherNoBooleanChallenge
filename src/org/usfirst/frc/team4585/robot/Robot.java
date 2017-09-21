805affebea3cfcd2f463eb592c9e8565c217add7
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2017-09-21 13:05:46 -0600

    removed while loops also

--- src/org/usfirst/frc/team4585/robot/Robot.java
+++ src/org/usfirst/frc/team4585/robot/Robot.java
@@ -1,8 +1,8 @@
 package org.usfirst.frc.team4585.robot;
 
-import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.IterativeRobot;
 
-public class Robot extends SampleRobot {
+public class Robot extends IterativeRobot {
 
 	int driveLPort = 0;
 	int driveRPort = 1;
@@ -16,11 +16,6 @@ public class Robot extends SampleRobot {
 	int fireButton = 1;
 	int fireSafetyButton = 3;
 
-	long time;
-	long startTime;
-	long timeTaken;
-	int millisPerIteration = 5;
-
 	TankDrive chassis = new TankDrive(driveLPort, driveRPort);
 	Extreme3DPro joy = new Extreme3DPro(joystickPort);
 	Launcher gun = new Launcher(cannonTriggerPort);
@@ -31,38 +26,11 @@ public class Robot extends SampleRobot {
 	}
 
 	@Override
-	public void robotInit() {
-
+	public void teleopPeriodic() {
+		chassis.arcadeDrive(-joy.getZ(), joy.getY());
+		
+		gun.setFiring((int)(joy.getButton(fireButton)*joy.getButton(fireSafetyButton)));
+		climber.setClimbing(joy.getButton(climbButton) * joy.getThrottle());
 	}
 
-	@Override
-	public void autonomous() {
-
-	}
-
-	@Override
-	public void operatorControl() {
-		time = System.currentTimeMillis();
-		while (isEnabled() & isOperatorControl()) {
-			while (System.currentTimeMillis() >= time + millisPerIteration) {
-				startTime = System.currentTimeMillis();
-
-				chassis.arcadeDrive(-joy.getZ(), joy.getY());
-				// chassis.arcadeDrive(-keyboard.getAxisAD(), keyboard.getAxisWS());
-
-				// gun.setFiring(joy.getButton(fireButton)&&joy.getButton(fireSafetyButton));
-
-				climber.setClimbing(joy.getButton(climbButton)*joy.getThrottle());
-
-				time = System.currentTimeMillis();
-
-				timeTaken = time - startTime;
-			}
-		}
-	}
-
-	@Override
-	public void test() {
-
-	}
 }
